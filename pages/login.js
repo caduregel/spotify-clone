b5cf7d73550429c4680c971683c0cd1a435c73aa
caduregel <liam@nehoray.net>
2022-05-08 19:51:05 +0200

    added jey at loginjs

--- pages/login.js
+++ pages/login.js
@@ -4,8 +4,8 @@ function login({ providers = {} }) {
     return (
         <div className="flex flex-col items-center bg-black min-h-screen w-full justify-center">
             <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt=""></img>
-            {Object.values(providers).map((provider) => (
-                <div>
+            {Object.values(providers).map((provider, index) => (
+                <div key={index}>
                     <button 
                     className="bg-[#18D860] text-white p-5 rounded-lg"
                     onClick={() => signIn(provider.id, { callbackUrl: '/'})}>
