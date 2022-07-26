04c9c66ffb0a85550664824fc336e404057ca5ce
caduregel <liam@nehoray.net>
2022-07-26 16:00:02 +0200

    Bug Fix

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -33,7 +33,7 @@ export default NextAuth({
     // Configure one or more authentication providers
     providers: [
         SpotifyProvider({
-            clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+            clientId: process.env._CLIENT_ID,
             clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
             authorization: LOGIN_URL,
         }),
